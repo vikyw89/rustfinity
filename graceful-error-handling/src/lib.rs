7b269cb0a7113875e9c70ba1321bb9427be2fbff
vikyw <vikyw89@gmail.com>
2025-01-02 00:36:39 +0800

    feat: error handling

---
+++ graceful-error-handling/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn parse_percentage(input: &str) -> Result<u8, String> {
+    // TODO: Implement the function here
+    let parsed_number = match input.parse::<i32>() {
+        Ok(number) => number,
+        Err(_) => return Err("Invalid input".to_string()),
+    };
+
+    if parsed_number < 0 || parsed_number > 100 {
+        return Err("Percentage out of range".to_string());
+    }
+
+    return Ok(parsed_number as u8);
+}
--- the-from-trait/src/lib.rs
+++ the-from-trait/src/lib.rs
@@ -5,11 +5,27 @@ pub struct Days(pub i32);
 impl From<Minutes> for Hours {
     fn from(minutes: Minutes) -> Hours {
         // Implement the minute to hour conversion here
+        return Hours(minutes.0 / 60);
     }
 }
 
 // TODO: implement from hours to days
+impl From<Hours> for Days {
+    fn from(hours: Hours) -> Days {
+        return Days(hours.0 / 24);
+    }
+}
 
 // TODO: implement from minutes to days
+impl From<Minutes> for Days {
+    fn from(minutes: Minutes) -> Days {
+        return Days(minutes.0 / 1440);
+    }
+}
 
 // TODO: implement from days to hours
+impl From<Days> for Hours {
+    fn from(days: Days) -> Hours {
+        return Hours(days.0 * 24);
+    }
+}
